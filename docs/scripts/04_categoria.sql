ee682503cbae6003654b445c5323b996dbf97de2
Vini-19 <marcosanchez-1@outlook.com>
2025-06-28 11:41:35 -0600

    correciones

--- docs/scripts/04_categoria.sql
+++ docs/scripts/04_categoria.sql
@@ -1,6 +1,8 @@
 use ecommerse;
-CREATE TABLE categoria(  
+CREATE TABLE categorias(  
     id int NOT NULL PRIMARY KEY AUTO_INCREMENT ,
     categoria VARCHAR(255),
     estado char(3)
 ) COMMENT 'Categoría de productos';
+
+
